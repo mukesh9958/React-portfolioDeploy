a514c2e17938e35b3752279b6848b467457a9add
Mukesh Kumar <49620364+mukesh9958@users.noreply.github.com>
2022-07-18 00:03:06 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -43,11 +43,11 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Problem Solving</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Everything</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
